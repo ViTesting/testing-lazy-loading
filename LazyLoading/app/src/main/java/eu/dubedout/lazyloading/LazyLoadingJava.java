cda5c4256fa14e5a6a8ff833561d6169e004a1b7
Vincent Dubedout <vincent.dubedout@gmail.com>
2016-02-13 18:56:33 -0500

    adding add instance, add lazy, get instance

--- LazyLoading/app/src/main/java/eu/dubedout/lazyloading/LazyLoadingJava.java
+++ LazyLoading/app/src/main/java/eu/dubedout/lazyloading/LazyLoadingJava.java
@@ -1,5 +1,8 @@
 package eu.dubedout.lazyloading;
 
+import android.os.Debug;
+import android.util.Log;
+
 import java.util.concurrent.ConcurrentHashMap;
 
 @SuppressWarnings("unchecked")
@@ -15,17 +18,49 @@ public class LazyLoadingJava {
         T get();
     }
 
-    public <T> void add(T instance) {
-        instanciatedObjectMap.put(instance.getClass(), instance);
+    public <T> void addInstance(Class clazz, T instance) {
+        if (instanciatedObjectMap.containsKey(clazz)) {
+            Log.w(this.getClass().toString(), "Instance already added in the list, erasing the previous one");
+        }
+
+        instanciatedObjectMap.put(clazz, instance);
     }
 
-    public void addLazy(LazySetter lazySetter) {
+    public <T> void addLazy(Class clazz, LazySetter<T> lazySetter) {
+        if (lazyObjectMap.containsKey(clazz)) {
+            Log.w(this.getClass().toString(), "LazySetter already added to the list, erasing the previous one");
+            if (instanciatedObjectMap.containsKey(clazz)) {
+                instanciatedObjectMap.remove(clazz);
+            }
+        }
 
+        lazyObjectMap.put(clazz, lazySetter);
     }
 
 
     public <T> T get(Class<T> clazz) {
-        return (T) instanciatedObjectMap.get(clazz);
+        if (instanciatedObjectMap.containsKey(clazz)) {
+            return (T) instanciatedObjectMap.get(clazz);
+        } else if (lazyObjectMap.containsKey(clazz)) {
+            synchronized (clazz) {
+                createClassInstance(clazz);
+            }
+
+            if (instanciatedObjectMap.containsKey(clazz)) {
+                return (T) instanciatedObjectMap.get(clazz);
+            }
+        }
+
+        throw new NullPointerException("Object have not been added to the LazyLoading class");
+    }
+
+    private <T> void createClassInstance(Class<T> clazz) {
+        if (!instanciatedObjectMap.containsKey(clazz)) {
+            Object instance = lazyObjectMap.get(clazz).get();
+            if (instance != null) {
+                instanciatedObjectMap.put(clazz, instance);
+            }
+        }
     }
 
     public <T> void get(Class<T> clazz, ResponseHandler<T> responseHandler) {
--- LazyLoading/app/src/test/java/eu/dubedout/lazyloading/LazyLoadingJavaTest.java
+++ LazyLoading/app/src/test/java/eu/dubedout/lazyloading/LazyLoadingJavaTest.java
@@ -1,17 +1,30 @@
 package eu.dubedout.lazyloading;
 
+import android.util.Log;
+
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Matchers.any;
+import static org.powermock.api.mockito.PowerMockito.doNothing;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
+import static org.powermock.api.mockito.PowerMockito.when;
 
-
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({Log.class})
 public class LazyLoadingJavaTest {
+
     private LazyLoadingJava sut;
 
     @Before
     public void setUp() {
         sut = new LazyLoadingJava();
+        mockStatic(Log.class);
+        when(Log.w(any(String.class), any(String.class))).thenReturn(0);
     }
 
     @Test
@@ -22,7 +35,7 @@ public class LazyLoadingJavaTest {
     @Test
     public void testAdd_whenAddingString_thenReturnStringInstance() {
         String input = "test";
-        sut.add(input);
+        sut.addInstance(String.class, input);
         String result = sut.get(input.getClass());
 
         assertThat(result).isEqualTo(input);
@@ -30,8 +43,34 @@ public class LazyLoadingJavaTest {
 
     @Test
     public void testAdd_whenAddingTwoString_thenReturnLastOne(){
+        String input1 = "test";
+        sut.addInstance(String.class, input1);
 
+        String input2 = "test2";
+        sut.addInstance(String.class, input2);
+
+        String result = sut.get(String.class);
+
+        assertThat(result).isEqualTo(input2);
     }
+
+    @Test
+    public void testAddLazy_whenAddingString_thenReturnInstanciatedString() {
+        final String input = new String("input1");
+
+        sut.addLazy(String.class, new LazyLoadingJava.LazySetter() {
+            @Override
+            public Object get() {
+                return input;
+            }
+        });
+
+        String result = sut.get(String.class);
+
+        assertThat(result).isEqualTo(input);
+    }
+
+
 
 
 }
